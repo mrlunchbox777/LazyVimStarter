838bad4a8563f2731be3a525395b087619f56a69
Andrew Shoell <mrlunchbox777@gmail.com>
2025-12-16 08:39:12 -0700

    fix: forgot the extra bit

--- init.lua
+++ init.lua
@@ -1,10 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- Workaround for checkhealth hanging in nvim 0.11.5
--- Use :Health [module] instead of :checkhealth
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    require("config.healthcheck-fix")
-  end,
-})
